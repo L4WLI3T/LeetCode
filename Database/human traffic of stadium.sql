466743dd1977d56524f19487aabdce1780be46c0
Amey T <48093400+L4WLI3T@users.noreply.github.com>
2024-05-26 16:38:21 +0530

    Update human traffic of stadium.sql

--- Database/human traffic of stadium.sql
+++ Database/human traffic of stadium.sql
@@ -1,3 +1,4 @@
+--Method 1
 select distinct S.id,S.visit_date,S.people from (
 select
 case when 
@@ -10,3 +11,18 @@ cross join stadium sld
 where slg.id=s.id-1 and sld.id=s.id+1) T
 join Stadium S on S.id in (select value from string_split(T.sid,','))
 where sid is not null
+--Method 2
+with temp as (
+select *,row_number() Over(order by id) as RN
+,id-row_number() Over(order by id) as id_count
+from dbo.stadium
+where people>=100)
+select id,visit_date,people from temp T
+Where Exists
+(
+	select id_count
+	from temp T2
+	where T2.id_count=T.id_count
+	group by id_count
+	having count(*)>=3
+)
